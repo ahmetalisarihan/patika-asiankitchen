c3ef3d52bf4b8cd27659480b97a4b7398e8295d2
Ahmet Ali Sarihan <ahmetalisarihan.com>
2023-01-08 00:00:00 +0300

    foodRecipe function

--- app.js
+++ app.js
@@ -94,3 +94,22 @@ const btnCreate = () => {
   `
   btnDOM.innerHTML = buttons;
 }
+btnCreate();
+
+const foodRecipes = (item) => {
+  let food = `
+    <div class="menu-items col-lg-6 col-sm-12">
+      <img src="${item.img}" alt="${item.title}" class="photo">
+      <div class="menu-info">
+        <div class="menu-title">
+          <h4>${item.title}</h4>
+          <h4 class="price">${item.price}</h4>
+        </div>
+        <div class="menu-text">
+          ${item.desc}
+        </div>
+      </div>
+    </div> 
+  `
+  return food;
+}
